5f0ce7e4bade4bc089e124dae1c7856a6feeec7d
k725 <k725@users.noreply.github.com>
2014-04-13 03:15:11 +0900

    Update function.php

--- lib/function.php
+++ lib/function.php
@@ -1,5 +1,5 @@
 <?php
-	require_once('lib/setting.php');
+	require_once(dirname(__FILE__).'/setting.php');
 
 	function ConnectSQL () {
 		return 'mysql:host='.Setting::DB_HostName.'; dbname='.Setting::DB_DBName.'; charset='.Setting::DB_Charset.';';
